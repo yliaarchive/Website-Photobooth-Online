774786b934e871fb66f04087da58f6ffcbb9acbc
sui <suinurhidayatusoleha07@gmail>
2026-06-16 13:51:50 +0900

    update kode

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::livewire('users', 'pages::user.index')->name('user.index');
 });
 
 require __DIR__.'/settings.php';
