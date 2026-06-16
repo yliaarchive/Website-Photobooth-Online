d3d72cfea83555498e9ae7b3602c213d140fef33
sui <suinurhidayatusoleha07@gmail>
2026-06-16 14:06:10 +0900

    update kode

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -14,8 +14,6 @@
                 <flux:sidebar.group :heading="__('Platform')" class="grid">
                     <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
-
-                    </flux:sidebar.item>
             
                     </flux:sidebar.item>
                 </flux:sidebar.group>
